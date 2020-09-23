2ac2689eb9f19a8b996036d0652d62e0b3153382
VaidasSt <vaidas.stumbras@gmail.com>
2020-09-23 14:21:19 +0300

    Update ContactFormController.php

--- src/Http/controllers/ContactFormController.php
+++ src/Http/controllers/ContactFormController.php
@@ -2,7 +2,7 @@
 
 namespace Samuelayox\Contactform\Http\Controllers;
 
-use Samuelayo\Contactform\Models\ContactForm;
+use Samuelayox\Contactform\Models\ContactForm;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
