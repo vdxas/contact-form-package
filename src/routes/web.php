588048d92837f978c954899a5ffe91fb3f9ec28a
VaidasSt <vaidas.stumbras@gmail.com>
2020-09-23 14:09:12 +0300

    Update web.php

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,6 +1,6 @@
 
 <?php
-Route::group(['namespace' => 'Samuelayo\Contactform\Http\Controllers', 'middleware' => ['web']], function(){
+Route::group(['namespace' => 'Samuelayox\Contactform\Http\Controllers', 'middleware' => ['web']], function(){
     Route::get('contact', 'ContactFormController@index');
     Route::post('contact', 'ContactFormController@sendMail')->name('contact');
 });
